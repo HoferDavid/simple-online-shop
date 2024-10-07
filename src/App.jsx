6075776e40125b55beab25aac4936144907b5d37
David Hofer <chassis.poppets0x@icloud.com>
2024-10-07 11:18:00 +0200

    Add to Basket

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,69 @@
 import React, { Component } from 'react';
 import Product from './components/product';
 import Navbar from './components/navbar';
+import ShoppingCart from './components/shopping-cart';
 
 class App extends Component {
-  state = {};
+  state = {
+    items: [],
+  };
+
+  addItem = (amount, name, price) => {
+    let currentItems = this.state.items;
+
+    let existingItems = this.state.items.find(item => item.name == name);
+    if (existingItems) {
+      existingItems.amount++;
+    } else {
+      currentItems.push({
+        amount,
+        name,
+        price,
+      });
+    }
+
+    this.setState({ items: currentItems });
+    console.log(this.state);
+  };
+
   render() {
     return (
       <React.Fragment>
         <Navbar />
-        <div className="product-container">
-          <Product
-            title="Strawberries"
-            img="strawberries.jpg"
-            description="Add Strawberries to your basket"
-          />
-          <Product
-            title="Apples"
-            img="apples.jpg"
-            description="Add Apples to your basket"
-          />
-          <Product
-            title="Mangos"
-            img="mangos.jpg"
-            description="Add Mangos to your basket"
-          />
-          <Product
-            title="Blueberries"
-            img="blueberries.jpg"
-            description="Add Blueberries to your basket"
-          />
-          <Product
-            title="Bananas"
-            img="bananas.jpg"
-            description="Add Bananas to your basket"
-          />
+
+        <div className="main-container">
+          <div className="product-container">
+            <Product
+              onAdd={() => this.addItem(1, 'Strawberries', 4.9)}
+              title="Strawberries"
+              img="strawberries.jpg"
+              description="Add Strawberries to your basket"
+            />
+            <Product
+              onAdd={() => this.addItem(1, 'Apples', 1.9)}
+              title="Apples"
+              img="apples.jpg"
+              description="Add Apples to your basket"
+            />
+            <Product
+              onAdd={() => this.addItem(1, 'Mangos', 10.9)}
+              title="Mangos"
+              img="mangos.jpg"
+              description="Add Mangos to your basket"
+            />
+            {/* <Product
+              title="Blueberries"
+              img="blueberries.jpg"
+              description="Add Blueberries to your basket"
+            /> */}
+            {/* <Product
+              title="Bananas"
+              img="bananas.jpg"
+              description="Add Bananas to your basket"
+            /> */}
+          </div>
+
+          <ShoppingCart items={this.state.items} />
         </div>
       </React.Fragment>
     );
---
+++ src/components/shopping-cart.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class ShoppingCart extends Component {
+  state = { };
+  render() {
+    return (
+      <div className="shopping-cart">
+        <h2>Shopping Cart</h2>
+        {this.props.items.map((item) => (
+          <div key={item.name}>
+            {item.amount} {item.name} {item.price}€
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default ShoppingCart;
